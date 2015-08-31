9c1c342330927d956c13ee4f2f11c7e4a661b529
3KCC <minhtranglamthi@gmail.com>
2015-08-31 15:15:55 +0800

    Create mapper.py

---
+++ mapper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+import re
+
+for line in sys.stdin:
+    data = line.strip().split("\"")
+    if len(data) == 3:
+    	request = data[1].strip().split(" ")
+    	if len(request) == 3:
+    		method, path, protocol = request
+    		path = re.sub('(http:\/\/)[^\/]{0,}','',path)
+	    	print "{0}".format( path if path else '/')
+	    	# print "{0}".format(re.sub('http://www.the-associates.co.uk', '', path))
+	    	# part1 = filter(None, re.split('\\[|\\]', data[0].strip()))
+	    	# if len(part1) == 2:
+	    	# 	part1a = part1[0].strip().split(" ")
+	    	# 	time = part1[1]
+	    	# 	if len(part1a) == 3:
+	    	# 		ip, clientId, username = part1a
+	    	# 		print "{0}".format(ip)
